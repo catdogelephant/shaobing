7f7452f6e2155e6694f631730e9a5d7f51154749
sunxiao <13964402805@36kr.com>
2022-04-20 11:45:17 +0800

    整合Spring Cloud Alibaba和Nacos

--- src/main/java/com/zhumuchang/dongqu/BatongxianInitializr01Application.java
+++ src/main/java/com/zhumuchang/dongqu/BatongxianInitializr01Application.java
@@ -3,9 +3,14 @@ package com.zhumuchang.dongqu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @EnableDiscoveryClient 服务发现
+ */
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BatongxianInitializr01Application {
 
     public static void main(String[] args) {
